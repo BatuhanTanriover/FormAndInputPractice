f72efa16e5751fbb026243faac8a2d3965678ee8
BatuhanTanriover <batu_2000_10@hotmail.com>
2024-11-11 22:55:50 +0300

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,63 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("")
+  // const [enteredPassword, setEnteredPassword] = useState("")
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const emailIsInvalid = 
+  enteredValues.email !== "" && !enteredValues.email.includes("@")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("basıldı");
+    console.log(enteredValues);
+  };
+
+  function handleInputChange(idetifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [idetifier]: value,
+    }));
+  }
+
+  // const handleEmailChange = (event) => {
+  //   setEnteredEmail(event.target.value);
+  // };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            value={enteredValues.email}
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+          />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email adress</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            value={enteredValues.password}
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+          />
         </div>
       </div>
 
